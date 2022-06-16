658ef4ec2d1e794e8adb38050bb61c52f77ef0d6
Balli Asghar <ballihacker@outlook.com>
2022-06-16 05:38:52 +0100

    init

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 import express from "express";
-import cors from "cors";
 import Database from "./db.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 
 Database.connectDB();
-
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
 
 app.get("/messages", async (req, res) => {
   try {
     const messages = await Database.Messages.find();
+
     res.json(messages);
   } catch (error) {
     res.sendStatus(500).json({ message: error.message });
@@ -22,6 +19,7 @@ app.get("/messages", async (req, res) => {
 app.post("/messages", async (req, res) => {
   try {
     const message = await Database.Messages.create(req.body);
+    console.log("message", message);
     res.json(message);
   } catch (error) {
     res.sendStatus(500).json({ message: error.message });
--- db.js
+++ db.js
@@ -10,7 +10,7 @@ const Messages = mongoose.model("Messages", MessagesSchema);
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/messages");
+    const conn = await mongoose.connect("mongodb://mongo:27017/messages");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(err);
